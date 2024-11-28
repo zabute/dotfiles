b3a29a0c676e72dd33f4c811352fdd64e4295157
zabute <arde987@gmail.com>
2024-11-28 18:32:10 +0300

    feat(nvim): remap G to Gzz

--- dot-config/nvim/lua/custom/keybinds.lua
+++ dot-config/nvim/lua/custom/keybinds.lua
@@ -1,6 +1,7 @@
 return {
   vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true }),
   vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', 'G', 'Gzz', { noremap = true, silent = true }),
 
   vim.api.nvim_set_keymap('n', '<C-k>', ':m .-2<CR>==', { noremap = true, silent = true }),
   vim.api.nvim_set_keymap('i', '<C-k>', '<Esc>:m .-2<CR>==gi', { noremap = true, silent = true }),
