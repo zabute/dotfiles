a248c0840b00ad06901ffa5895aba36b7d61743e
zabute <arde987@gmail.com>
2024-11-29 18:56:23 +0300

    fix(nvim): fix harpoon keymaps

--- dot-config/nvim/lua/custom/plugins/harpoon.lua
+++ dot-config/nvim/lua/custom/plugins/harpoon.lua
@@ -23,24 +23,20 @@ return {
       harpoon:list():clear()
     end)
 
-    vim.keymap.set('n', '<C-a>', function()
+    vim.keymap.set('n', '<leader>a', function()
       harpoon:list():select(1)
-      vim.cmd 'normal! zz'
     end)
 
-    vim.keymap.set('n', '<C-s>', function()
+    vim.keymap.set('n', '<leader>s', function()
       harpoon:list():select(2)
-      vim.cmd 'normal! zz'
     end)
 
-    vim.keymap.set('n', '<C-d>', function()
+    vim.keymap.set('n', '<leader>d', function()
       harpoon:list():select(3)
-      vim.cmd 'normal! zz'
     end)
 
-    vim.keymap.set('n', '<C-f>', function()
+    vim.keymap.set('n', '<leader>f', function()
       harpoon:list():select(4)
-      vim.cmd 'normal! zz'
     end)
   end,
 }
