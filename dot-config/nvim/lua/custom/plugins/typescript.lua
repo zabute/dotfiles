341e4f62f7c9097e886739d82cb5609ba04b291b
zabute <arde987@gmail.com>
2024-11-29 11:43:34 +0300

    fix(nvim): use tailwindcss lsp

--- dot-config/nvim/lua/custom/plugins/typescript.lua
+++ dot-config/nvim/lua/custom/plugins/typescript.lua
@@ -15,4 +15,15 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {},
   },
+  {
+    'luckasRanarison/tailwind-tools.nvim',
+    name = 'tailwind-tools',
+    build = ':UpdateRemotePlugins',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-telescope/telescope.nvim', -- optional
+      'neovim/nvim-lspconfig', -- optional
+    },
+    opts = {}, -- your configuration
+  },
 }
