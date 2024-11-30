5894dffd789d8604157b61fde585845e1c9f8588
zabute <arde987@gmail.com>
2024-11-30 22:14:32 +0300

    fix(nvim): redo harpoon keymaps

--- dot-config/nvim/lua/custom/plugins/harpoon.lua
+++ dot-config/nvim/lua/custom/plugins/harpoon.lua
@@ -7,32 +7,57 @@ return {
     local harpoon = require 'harpoon'
     harpoon:setup {}
 
-    vim.keymap.set('n', '<leader>h', function()
+    vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
     end)
 
-    vim.keymap.set('n', '<leader>j', function()
+    vim.keymap.set('n', '<leader>x', function()
       harpoon:list():remove()
     end)
 
-    vim.keymap.set('n', '<C-e>', function()
+    vim.keymap.set('n', '<leader>e', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
-    vim.keymap.set('n', '<C-q>', function()
+    vim.keymap.set('n', '<leader>q', function()
       harpoon:list():clear()
     end)
 
-    vim.keymap.set('n', '<leader>a', function()
+    vim.keymap.set('n', '<leader>1', function()
       harpoon:list():select(1)
     end)
 
-    vim.keymap.set('n', '<leader>s', function()
+    vim.keymap.set('n', '<leader>2', function()
       harpoon:list():select(2)
     end)
 
-    vim.keymap.set('n', '<leader>d', function()
+    vim.keymap.set('n', '<leader>3', function()
       harpoon:list():select(3)
+    end)
+
+
+    vim.keymap.set('n', '<leader>4', function()
+      harpoon:list():select(4)
+    end)
+
+    vim.keymap.set('n', '<leader>5', function()
+      harpoon:list():select(5)
+    end)
+
+    vim.keymap.set('n', '<leader>6', function()
+      harpoon:list():select(6)
+    end)
+
+    vim.keymap.set('n', '<leader>7', function()
+      harpoon:list():select(7)
+    end)
+
+    vim.keymap.set('n', '<leader>8', function()
+      harpoon:list():select(8)
+    end)
+
+    vim.keymap.set('n', '<leader>9', function()
+      harpoon:list():select(9)
     end)
   end,
 }
