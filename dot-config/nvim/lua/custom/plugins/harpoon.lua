86b2c4d9f56b24225e0f1467a6d68e520604fd73
zabute <arde987@gmail.com>
2024-12-01 00:28:03 +0300

    fix(nvim): add keymap descriptions for harpoon

--- dot-config/nvim/lua/custom/plugins/harpoon.lua
+++ dot-config/nvim/lua/custom/plugins/harpoon.lua
@@ -9,54 +9,54 @@ return {
 
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
-    end)
+    end, { desc = 'Harpoon add current' })
 
     vim.keymap.set('n', '<leader>x', function()
       harpoon:list():remove()
-    end)
+    end, { desc = 'Harpoon remove current' })
 
     vim.keymap.set('n', '<leader>e', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    end, { desc = 'Toggle Harpoon quick menu' })
 
     vim.keymap.set('n', '<leader>Q', function()
       harpoon:list():clear()
-    end)
+    end, { desc = 'Harpoon clear all' })
 
     vim.keymap.set('n', '<leader>1', function()
       harpoon:list():select(1)
-    end)
+    end, { desc = 'Harpoon select 1' })
 
     vim.keymap.set('n', '<leader>2', function()
       harpoon:list():select(2)
-    end)
+    end, { desc = 'Toggle harpoon quick menu' })
 
     vim.keymap.set('n', '<leader>3', function()
       harpoon:list():select(3)
-    end)
+    end, { desc = 'Harpoon select 3' })
 
     vim.keymap.set('n', '<leader>4', function()
       harpoon:list():select(4)
-    end)
+    end, { desc = 'Harpoon select 4' })
 
     vim.keymap.set('n', '<leader>5', function()
       harpoon:list():select(5)
-    end)
+    end, { desc = 'Harpoon select 5' })
 
     vim.keymap.set('n', '<leader>6', function()
       harpoon:list():select(6)
-    end)
+    end, { desc = 'Harpoon select 6' })
 
     vim.keymap.set('n', '<leader>7', function()
       harpoon:list():select(7)
-    end)
+    end, { desc = 'Harpoon select 7' })
 
     vim.keymap.set('n', '<leader>8', function()
       harpoon:list():select(8)
-    end)
+    end, { desc = 'Harpoon select 8' })
 
     vim.keymap.set('n', '<leader>9', function()
       harpoon:list():select(9)
-    end)
+    end, { desc = 'Harpoon select 9' })
   end,
 }
