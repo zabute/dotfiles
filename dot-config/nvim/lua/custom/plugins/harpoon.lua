fed905ec7916c2219a06bf6d8d26b9a2214f0a86
zabute <arde987@gmail.com>
2024-12-03 11:08:12 +0300

    fix(nvim): use telescope ui

--- dot-config/nvim/lua/custom/plugins/harpoon.lua
+++ dot-config/nvim/lua/custom/plugins/harpoon.lua
@@ -7,6 +7,41 @@ return {
     local harpoon = require 'harpoon'
     harpoon:setup {}
 
+    -- Slightly modified telescope ui from the docs.
+    -- This adds an index in the list for the file so i can filter the list by pressing 1-9
+    local conf = require('telescope.config').values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for index, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, {
+          display = string.format('%d: %s', index, item.value),
+          path = item.value,
+        })
+      end
+
+      require('telescope.pickers')
+        .new({}, {
+          prompt_title = 'Harpoon',
+          finder = require('telescope.finders').new_table {
+            results = file_paths,
+            entry_maker = function(entry)
+              return {
+                value = entry.path,
+                display = entry.display,
+                ordinal = entry.display,
+              }
+            end,
+          },
+          previewer = conf.file_previewer {},
+          sorter = conf.generic_sorter {},
+        })
+        :find()
+    end
+
+    vim.keymap.set('n', '<leader>e', function()
+      toggle_telescope(harpoon:list())
+    end, { desc = 'Open harpoon window' })
+
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
     end, { desc = 'Harpoon add current' })
@@ -15,9 +50,9 @@ return {
       harpoon:list():remove()
     end, { desc = 'Harpoon remove current' })
 
-    vim.keymap.set('n', '<leader>e', function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end, { desc = 'Toggle Harpoon quick menu' })
+    -- vim.keymap.set('n', '<leader>e', function()
+    --   harpoon.ui:toggle_quick_menu(harpoon:list())
+    -- end, { desc = 'Toggle Harpoon quick menu' })
 
     vim.keymap.set('n', '<leader>Q', function()
       harpoon:list():clear()
